4b6f1ba56b96e1a43a11b4416c1397f5ec7674bb
AvraamNikolaou <avnikolaou@hotmail.com>
2018-11-08 16:48:10 +0200

    Added: Created 'move to top button' and it's functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+// When the user clicks on a section, page scrolls down
 function scrollToDiv() {
 
     let section = "section";
@@ -5,9 +6,25 @@ function scrollToDiv() {
     let destination = section.concat(id);
 
     let e = document.getElementById(destination);
-    console.log(e);
     if (!!e && e.scrollIntoView) {
         e.scrollIntoView();
     }
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
