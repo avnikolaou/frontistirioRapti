df8cd3ba7b7c56fa8311c8f1b23aa7a8ee80880a
AvraamNikolaou <avnikolaou@hotmail.com>
2019-03-20 01:14:07 +0200

    Fixed: Slider show now checks if there are slides

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -48,22 +48,24 @@ function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
+    if (slides.length > 0) {
+        if (n > slides.length) {
+            slideIndex = 1
+        }
 
-    if (n < 1) {
-        slideIndex = slides.length
-    }
+        if (n < 1) {
+            slideIndex = slides.length
+        }
 
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
 }
 
 
